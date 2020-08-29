4d9d5761c3a5c59e72eb20c41018de5432f3227c
Mr Skeleker <hskeleker@gmail.com>
2020-08-29 12:28:26 +0300

    Updates

---
+++ precache-manifest.a1b486f967bd834949896bce8c9391d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7e2e57fd41bca5c30ea71bf4a31414",
+    "url": "/React-App-Template/index.html"
+  },
+  {
+    "revision": "db975f1bd72c55d4fcc4",
+    "url": "/React-App-Template/static/css/main.61c36e36.chunk.css"
+  },
+  {
+    "revision": "2fa9f0c00f7c487b6968",
+    "url": "/React-App-Template/static/js/2.67af2b83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-App-Template/static/js/2.67af2b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db975f1bd72c55d4fcc4",
+    "url": "/React-App-Template/static/js/main.e9fe60e0.chunk.js"
+  },
+  {
+    "revision": "9dd5d6ad1e55d4a1f697",
+    "url": "/React-App-Template/static/js/runtime-main.7831ae9d.js"
+  }
+]);
